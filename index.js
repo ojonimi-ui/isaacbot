b6340539e6f22906bc251d4ac4ace5d594f82400
ojonimi-ui <josephojonimi21@gmail.com>
2024-11-09 07:59:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const keep_alive = require('./keep_alive.js');
 
 // Replace with your bot's token
-const token = '8198239466:AAHJsQvUQtQb78VfG-RGO4jYMwXQrbYT27k';
+const token = '8198239466:AAFt02CmlifQMiNBrzwDtJUkDYg4ujtciVw';
 
 // Exchange rates
 const SOL_TO_NGN = 236953; // 1 SOL in NGN
